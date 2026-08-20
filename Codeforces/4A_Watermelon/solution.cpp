e5bac8e12c3957c6dfd597b192a2f217ca5e5fd8
shaikasadulla-cp <shaik.asadulla.off@gmail.com>
2026-08-21 03:01:57 +0530

    Solve Codeforces 4A - Watermelon

--- Codeforces/4A_Watermelon/solution.cpp
+++ Codeforces/4A_Watermelon/solution.cpp
@@ -1,1 +1,15 @@
-// Solution code not added
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+ 
+    if(n % 2 == 0 && n > 2) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+    return 0;
+}
