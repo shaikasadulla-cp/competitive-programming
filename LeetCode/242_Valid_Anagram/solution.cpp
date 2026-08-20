7ab7ed08b81f1553ebba63fc1e7aad12cf4dcd3b
shaikasadulla-cp <shaik.asadulla.off@gmail.com>
2026-08-21 04:52:39 +0530

    Solve LeetCode 242 - Valid Anagram

---
+++ LeetCode/242_Valid_Anagram/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        int freq[26] = {0};
+        for(char c : s) {
+            freq[c - 'a']++;
+        }
+        for(char c : t) {
+            freq[c - 'a']--;
+        }
+        for(int i = 0; i < 26; i++) {
+            if(freq[i] != 0) return false;
+        }
+        return true;
+    }
+};
